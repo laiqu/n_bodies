7e19db5b5f185776fbd39792d3394bebfefd1ed0
n3v <thezbylut@gmail.com>
2015-01-28 00:32:00 +0100

    Setting position to center of mass while merging objects.

--- n_bodies.cu
+++ n_bodies.cu
@@ -81,6 +81,8 @@ void glue_nearby(K* bodies, int n, int dims) {
             if (i == j) continue;
             if (is_nearby(BODY(bodies, i), BODY(bodies, j), dims) &&
                     MASS(BODY(bodies, i)) >= MASS(BODY(bodies, j))) {
+               for (int k=0; k<dims; ++k)
+                   POS(BODY(bodies, i))[k] = (POS(BODY(bodies, i))[k]*MASS(BODY(bodies, i)) + POS(BODY(bodies, j))[k]*MASS(BODY(bodies, j))) / (MASS(BODY(bodies, i)) + MASS(BODY(bodies, j)));
                MASS(BODY(bodies, i)) += MASS(BODY(bodies, j));
                MASS(BODY(bodies, j)) = 0;
                RADI(BODY(bodies, i)) += RADI(BODY(bodies, j));
